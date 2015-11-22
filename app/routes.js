25712c6c217981d00f0f239aed0c4efed6577956
Todmy <tolok.d@gmail.com>
2015-11-23 01:23:29 +0200

    Added simple articles implementation

--- app/routes.js
+++ app/routes.js
@@ -10,7 +10,7 @@ module.exports = function(app, passport) {
     res.render('index');
   });
 
-  app.get('/login', function(req, res) {
+  app.get('/login', isAuthorized, function(req, res) {
     res.render('auth', { message: req.flash('loginMessage') })
   });
 
@@ -20,7 +20,7 @@ module.exports = function(app, passport) {
     failureFlash: true
   }))
 
-  app.get('/signup', function(req, res) {
+  app.get('/signup', isAuthorized, function(req, res) {
     res.render('auth', { message: req.flash('signupMessage') })
   });
 
@@ -38,10 +38,30 @@ module.exports = function(app, passport) {
     req.logout();
     res.redirect('/')
   });
+
+  app.get('/article/:id', isLoggedIn, function(req, res) {
+    console.log('-------------------->', req.params.id) //
+    var article = {
+      id: '123dsfe2',
+      title: 'bla',
+      date: '23/11/2015 12:45:51',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      author: 'username_Dima'
+    }
+    res.render('article', { article: article })
+  })
 };
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect('/');
+}
+
+function isAuthorized(req, res, next) {
+  if (!req.isAuthenticated()) {
     return next();
   }
 
